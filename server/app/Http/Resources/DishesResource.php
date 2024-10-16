0bb1e630687ca711706e2fed714922abc5d82f95
DevShaded <devshaded@devshaded.com>
2024-10-16 14:47:25 +0200

    feat: add dishes resource

---
+++ server/app/Http/Resources/DishesResource.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Resources;
+
+use App\Models\Dish;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
+
+/** @mixin Dish */
+final class DishesResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'price' => $this->price,
+            'image_url' => Storage::url($this->image_url),
+        ];
+    }
+}
