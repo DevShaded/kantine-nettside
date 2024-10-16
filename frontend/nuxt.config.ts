e36ae86678ca55cf07a44b3f3079c195a66e48cc
DevShaded <devshaded@devshaded.com>
2024-10-16 09:25:38 +0200

    Init Nuxt Frontend

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/fonts',
+    '@nuxt/eslint',
+    'nuxt-seo-experiments',
+    '@nuxt/icon',
+    '@nuxtjs/color-mode',
+    '@nuxt/content'
+  ]
+})
