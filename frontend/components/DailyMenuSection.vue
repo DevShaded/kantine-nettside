175186a7e74c29d576ea2f466c13d92bd819f7a8
DevShaded <devshaded@devshaded.com>
2024-11-01 15:30:28 +0100

    refactor: make dishes into a component

--- frontend/components/DailyMenuSection.vue
+++ frontend/components/DailyMenuSection.vue
@@ -45,18 +45,11 @@ const { status, data: menu } = await useFetch(`http://server.test/api/menus/${da
 
       <div v-else>
         <div v-if="menu !== null" class="mt-6 grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-0 lg:gap-x-8">
-          <article v-for="dish in menu.data.dishes?.slice(0, 4)" :key="dish.id" class="group relative">
-            <div v-if="dish.image_url" class="h-56 w-full overflow-hidden rounded-md group-hover:opacity-75 lg:h-72 xl:h-80">
-              <NuxtImg :src="'http://server.test' + dish.image_url" :alt="dish.name" class="h-full w-full object-cover object-center" />
-            </div>
-            <h3 class="mt-4 text-sm text-zinc-700 dark:text-zinc-300">
-              <NuxtLink to="/meny">
-                <span class="absolute inset-0"></span>
-                {{ dish.name }}
-              </NuxtLink>
-            </h3>
-            <p class="mt-1 text-sm font-medium text-zinc-900 dark:text-zinc-400">{{ dish.price }},-</p>
-          </article>
+          <DishCard
+            :dish="dish"
+            v-for="dish in menu.data.dishes?.slice(0, 4)"
+            :key="dish.id"
+          />
         </div>
 
         <div v-else>
---
+++ frontend/components/DishCard.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+export interface DishEntity {
+  id: string;
+  name: string;
+  slug: string;
+  description: string;
+  price: number;
+  image_url?: string | null;
+}
+
+defineProps<{
+  dish: DishEntity
+}>();
+</script>
+
+<template>
+  <article class="group relative">
+    <div v-if="dish.image_url" class="h-56 w-full overflow-hidden rounded-md group-hover:opacity-75 lg:h-72 xl:h-80">
+      <NuxtImg :src="'http://server.test' + dish.image_url" :alt="dish.name" class="h-full w-full object-cover object-center" />
+    </div>
+    <h3 class="mt-4 text-sm text-zinc-700 dark:text-zinc-300">
+      <NuxtLink to="/meny">
+        <span class="absolute inset-0"></span>
+        {{ dish.name }}
+      </NuxtLink>
+    </h3>
+    <p class="mt-1 text-sm font-medium text-zinc-900 dark:text-zinc-400">{{ dish.price }},-</p>
+  </article>
+</template>
+
+<style scoped></style>
