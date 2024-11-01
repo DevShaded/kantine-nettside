71b26aa0a5a3bc961abef360e13e12cb59434e27
DevShaded <devshaded@devshaded.com>
2024-11-01 15:25:09 +0100

    feat: add seo title and description

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,4 +1,13 @@
 <script setup lang="ts">
+useHead({
+  title: 'Hjem - Kantine',
+  meta: [
+    {
+      name: 'description',
+      content: 'Velkommen til kantinen! Vi serverer sunn og god mat til deg og dine kollegaer.'
+    }
+  ]
+});
 </script>
 
 <template>
